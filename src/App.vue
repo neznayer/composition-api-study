7e78c19f7619f9e71566861f9245ed7c8e12bf07
Anton <nokker@gmail.com>
2022-06-05 21:50:29 +0900

    Solution 1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div>Capacity: {{ capacity }}</div>
+  <p>Participants:</p>
+  <ul>
+    <li v-for="(participant, index) in participants" :key="index">
+      {{ participant }}
+    </li>
+  </ul>
+  <p>Spaces left: {{ spacesLeft }}</p>
+  <button @click="increaseCapacity()">Increase capacity</button>
 </template>
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 export default {
   setup() {
     const capacity = ref(3);
-    // additional code to write
-    return { capacity };
+    const participants = ref(["Spongebob", "Mr.Crabbs", "Squidward"]);
+
+    const spacesLeft = computed(
+      () => capacity.value - participants.value.length
+    );
+
+    function increaseCapacity() {
+      capacity.value++;
+    }
+    return { capacity, participants, spacesLeft, increaseCapacity };
   },
 };
 </script>
