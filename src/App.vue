5c91b9376619758aa3452e2c4bfe35282486f9f9
Anton <nokker@gmail.com>
2022-06-06 11:40:46 +0900

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>Capacity: {{ capacity }}</div>
+  <div>
+    Search for <input v-model="searchInput" />
+    <div>
+      <p>Number of animals: {{ results }}</p>
+    </div>
+  </div>
 </template>
 <script>
 import { ref } from "vue";
+import { getAnimalCount } from "@/api/animals";
+
 export default {
   setup() {
-    const capacity = ref(3);
-    // additional code to write
-    return { capacity };
+    const searchInput = ref("");
+    const results = ref(0);
+
+    getAnimalCount(searchInput.value).then((resp) => {
+      results.value = resp;
+    });
+
+    return { searchInput, results };
   },
 };
 </script>
---
+++ src/api/animals.js
@@ -0,0 +1,7 @@
+const baseURL = "http://192.168.86.90:3000";
+
+export async function getAnimalCount(searchInput) {
+  const response = await fetch(`${baseURL}/animals?title_like=${searchInput}`);
+  const data = await response.json();
+  return data.length;
+}
